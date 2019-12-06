caef973669ce6dd10b7cb778441a4f2750d5b5c6
Avin Vij <avin.vij@xoriant.com>
2019-12-06 21:20:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+io.on("connection", (mySocket) => {
+    console.log("user connected");
+    io.sockets.emit("newuser" , mySocket.id)
+    mySocket.on("disconnect", () => {
+        console.log("User disconnected");
+    })
+    mySocket.on("mystream", (msg) => {        
+        io.sockets.emit("mystream", mySocket.id+"@"+msg);
+    })
+})
+
+app.get("/", (req, res, next) => {
+    res.sendfile(__dirname + "/index.html");
+})
+
+http.listen(process.env.PORT || 9001, () => {
+    console.log("server started at 9001");
+})
